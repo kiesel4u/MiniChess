55e4d628cd8f5a29d793c821fceab04e61e1e570
Ralf Landwehr <ralle@Ralle-Notebook>
2015-05-06 17:03:06 +0200

    Client file added

---
+++ MiniChess/src/pawnWarsTest/OnlinePlayer.java
@@ -0,0 +1,15 @@
+/**
+ * @author Michael Kiesel and Ralf Landwehr
+ *
+ */
+package pawnWarsTest;
+import pawnWars.*;
+
+public class OnlinePlayer {
+
+	public static void main(String[] args) {
+		Client client = new Client(
+
+	}
+
+}
