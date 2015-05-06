6b4a8de91bebae21b0c34ba8e29235f51f5cea9f
Ralf Landwehr <ralle@Ralle-Notebook>
2015-05-06 17:29:20 +0200

    OnlinePlayer implemented

--- MiniChess/src/pawnWarsTest/OnlinePlayer.java
+++ MiniChess/src/pawnWarsTest/OnlinePlayer.java
@@ -3,13 +3,39 @@
  *
  */
 package pawnWarsTest;
+import java.io.IOException;
+
 import pawnWars.*;
 
 public class OnlinePlayer {
 
-	public static void main(String[] args) {
-		Client client = new Client(
+	public static void main(String[] args) throws IOException {
+			
+			State state = new State();
+			
+			PlayerModel player = new IterativeDeepeningNegaMaxPlayer();
+			
+			Move saveMove;
+			
+			Client client = new Client("imcs-wurzburg.svcs.cs.pdx.edu", "80", "Pawn", "pawnwars");
+			char c = client.offer('?');
+			
+			System.out.println("A new game starts ...");
+			
+			while(state.endOfTheGame == '?') {
+				if((c == 'W' && state.turn == 'W') || (c == 'B' && state.turn == 'B')) {
+					saveMove = player.getMove(state);
+					client.sendMove(saveMove.toString());
+					state.move(saveMove);
+				}
 
+				else if((c == 'W' && state.turn == 'B') || (c == 'B' && state.turn == 'W'))
+					state.move(new Move(client.getMove()));
+				
+			}
+			
+			System.out.println("Player " + state.endOfTheGame + " wins");
+			
 	}
 
 }
