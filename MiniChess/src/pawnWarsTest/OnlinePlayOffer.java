ac7b71f7ad4231c4bd6efe078224d2838c2f9b00
RalleEXA <RalleEXA@users.noreply.github.com>
2015-05-08 11:27:18 +0200

    Update OnlinePlayOffer.java

--- MiniChess/src/pawnWarsTest/OnlinePlayOffer.java
+++ MiniChess/src/pawnWarsTest/OnlinePlayOffer.java
@@ -17,7 +17,7 @@ public class OnlinePlayOffer {
 			
 			Move saveMove;
 			
-			Client client = new Client("imcs-wurzburg.svcs.cs.pdx.edu", "80", "Pawn", "pawnwars");
+			Client client = new Client("imcs.svcs.cs.pdx.edu", "3589", "Pawn", "pawnwars");
 			
 			System.out.println("possible Players:");
 			System.out.println("(1) RandomPlayer");
@@ -85,7 +85,14 @@ public class OnlinePlayOffer {
 				}
 
 				else if((c == 'W' && state.turn == 'B') || (c == 'B' && state.turn == 'W'))
+					try {
 						state.move(new Move(client.getMove()));
+					} catch (Exception e) {
+						//Catches NullPointerException, if game got interrupted.
+						//Do nothing at all(!), because no more moves are expected.
+					}
 			}
+			state.print();
+			System.out.println("Player " + state.endOfTheGame + " wins");
 	}
 }
