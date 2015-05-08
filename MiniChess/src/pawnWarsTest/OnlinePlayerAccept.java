85b5846f41e9698e9c30984fd5285284951e010e
Ralf Landwehr <ralle@Ralle-Notebook>
2015-05-08 10:45:39 +0200

    Remove try catch for getting latest server responses

--- MiniChess/src/pawnWarsTest/OnlinePlayerAccept.java
+++ MiniChess/src/pawnWarsTest/OnlinePlayerAccept.java
@@ -41,12 +41,8 @@ public class OnlinePlayerAccept {
 			}
 
 			else if((c == 'W' && state.turn == 'B') || (c == 'B' && state.turn == 'W'))
-				try {
 					state.move(new Move(client.getMove()));
-				} catch (Exception e) {
-					//Catches NullPointerException, if game got interrupted.
-					//Do nothing at all(!), because no more moves are expected.
-				}
+
 		}
 	}
 
